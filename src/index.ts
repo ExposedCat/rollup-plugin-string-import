7f2654f781b15e815d11a5ef2ee89309483bb959
Artem Prokop <artem13.prokop@gmail.com>
2024-08-12 21:31:11 +0200

    feat: base plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+import type { Plugin } from 'rollup';
+import { createFilter } from '@rollup/pluginutils';
+import type { FilterPattern } from '@rollup/pluginutils';
+
+export type ImportAsStringOptions = {
+  include: FilterPattern;
+  exclude?: FilterPattern;
+};
+
+export function importAsString(options: ImportAsStringOptions): Plugin {
+  const filter = createFilter(options.include, options.exclude);
+
+  return {
+    name: 'importAsString',
+    transform(code, id) {
+      if (filter(id)) {
+        return {
+          code: `export default ${JSON.stringify(code)};`,
+          map: { mappings: '' },
+        };
+      }
+    },
+  };
+}
